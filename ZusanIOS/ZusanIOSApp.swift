e1923d005e3099f73ca71664a8440cdfacd1161c
Sudharsan <sudharsan58@gmail.com>
2022-11-22 22:18:00 +0530

    Initial Commit

---
+++ ZusanIOS/ZusanIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZusanIOSApp.swift
+//  ZusanIOS
+//
+//  Created by Sudhar Zan on 22/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ZusanIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
