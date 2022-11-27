f557a2391dbeb81b19270eb22b0a855313e6484d
Sudharsan <sudharsan58@gmail.com>
2022-11-27 18:12:01 +0530

    Sample book   list view

--- ZusanIOS/ContentView.swift
+++ ZusanIOS/ContentView.swift
@@ -1,30 +1,41 @@
+
 import SwiftUI
-class ContentViewData: ObservableObject {
-    @Published var titleInput: String = ""
-    @Published var authorInput: String = ""
-}
+
 struct ContentView: View {
-    @ObservedObject var contentData = ContentViewData()
     @EnvironmentObject var appData: ApplicationData
     var body: some View {
-        VStack (spacing: 8) {
-            Text (appData.userData.header)
-                .padding (10)
-            TextField("Insert Title", text: $contentData.titleInput)
-                .textFieldStyle(.roundedBorder)
-            TextField("Insert Author", text: $contentData.authorInput)
-                .textFieldStyle(.roundedBorder)
-            Button(action: {
-                appData.userData.book.title = contentData.titleInput
-                appData.userData.book.author = contentData.authorInput
-            }, label: { Text ("Save") })
-            Spacer ()
-        }.padding()
+        ScrollViewReader{ proxy in
+        ScrollView{
+            ForEach(appData.userData) { book in
+                VStack {
+                    HStack(alignment: .top) {
+                        Image(book.cover)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 100)
+                        VStack(alignment: .leading, spacing: 2) {
+                            Text(book.title).bold()
+                            Text(book.author)
+                            Text(book.year).font(.caption)
+                        }.padding(.top, 5)
+                        Spacer()
+                    }.padding([.leading, .trailing], 10)
+                        .padding([.top, .bottom], 5)
+                    Divider()
+                }
+            }
+            Button("Top"){
+                if let firstIdentifier = appData.userData.first?.id{
+                    proxy.scrollTo(firstIdentifier, anchor:.top)
+                }
+            }
+        }
     }
+}
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            .environmentObject(ApplicationData())
+        ContentView().environmentObject(ApplicationData())
     }
 }
+
