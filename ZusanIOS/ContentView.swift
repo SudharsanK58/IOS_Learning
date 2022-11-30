1510ade57142de7e641f59d81f983ca8a4a7d1c3
Sudharsan <sudharsan58@gmail.com>
2022-11-30 22:42:00 +0530

    Book list

--- ZusanIOS/ContentView.swift
+++ ZusanIOS/ContentView.swift
@@ -2,40 +2,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject var appData: ApplicationData
-    var body: some View {
-        ScrollViewReader{ proxy in
-        ScrollView{
-            ForEach(appData.userData) { book in
-                VStack {
-                    HStack(alignment: .top) {
-                        Image(book.cover)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 80, height: 100)
-                        VStack(alignment: .leading, spacing: 2) {
-                            Text(book.title).bold()
-                            Text(book.author)
-                            Text(book.year).font(.caption)
-                        }.padding(.top, 5)
-                        Spacer()
-                    }.padding([.leading, .trailing], 10)
-                        .padding([.top, .bottom], 5)
-                    Divider()
-                }
+   @EnvironmentObject var appData: ApplicationData
+
+   var body: some View {
+      NavigationStack {
+         List(appData.userData) { book in
+            CellBook(book: book)
+         }
+         .navigationTitle(Text("Books"))
+         .navigationBarTitleDisplayMode(.inline)
+         .toolbar {
+            ToolbarItemGroup(placement: .primaryAction) {
+               Button(action: {
+                  print("Adding Book...")
+               }, label: {
+                  Image(systemName: "plus.app")
+               })
             }
-            Button("Top"){
-                if let firstIdentifier = appData.userData.first?.id{
-                    proxy.scrollTo(firstIdentifier, anchor:.top)
-                }
+            ToolbarItemGroup(placement: .secondaryAction) {
+               Button(action: {
+                  print("Sorting Books...")
+               }, label: {
+                  Label("Sort Books", systemImage: "arrow.up.arrow.down")
+               })
             }
-        }
-    }
+         }
+         .toolbarRole(.editor)
+      }
+   }
 }
+struct CellBook: View {
+   let book: BookViewModel
+
+   var body: some View {
+      HStack(alignment: .top) {
+         Image(book.cover)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 80, height: 100)
+         VStack(alignment: .leading, spacing: 2) {
+            Text(book.title).bold()
+            Text(book.author)
+            Text(book.year).font(.caption)
+            Spacer()
+         }.padding(.top, 5)
+         Spacer()
+      }
+   }
 }
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environmentObject(ApplicationData())
-    }
+   static var previews: some View {
+      ContentView().environmentObject(ApplicationData())
+   }
 }
 
